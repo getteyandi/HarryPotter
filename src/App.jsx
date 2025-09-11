fa20e391b70c70fc0b1848d2ac35ea407e6b94df
Jhanuz <jhanuz321@gmail.com>
2025-09-11 11:58:35 +0800

    Adds custom button and section components; refines layout
    Introduces a reusable button and enhances section title/detail components for improved UI consistency.
    Refactors layout structure and gradients, adds scroll-responsive top bar, and updates theme variables.
    Integrates autoprefixer for better CSS compatibility.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Clouds from "./components/clouds";
 import Birds from "./components/birds";
 import SectionTitle from "./components/sectionTitle";
 import SectionDetail from "./components/sectiondetail";
+import HpButton from "./components/hp-button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -23,21 +24,16 @@ function App() {
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis
             reprehenderit non, quod voluptates ipsam aliquid modi
           </p>
-
-          <button className="z-20 bg-hp-royal px-9  py-3 rounded-full mt-6 cursor-pointer font-bold hover:brightness-80 transition-all">
-            Start Journey
-          </button>
+          <HpButton text="Start Journey" className={"mt-6"} />
         </div>
-
         <Birds />
         <Clouds />
-
         <div className="absolute w-full h-20 bg-gradient-to-t from-hp-royal to-transparent left-0 bottom-0 z-20" />
       </section>
 
-      <section className="h-screen bg-[linear-gradient(225deg,#041119_8%,#041119_60%,#000103_100%)] pt-16">
+      {/* Characters Section */}
+      <section className="relative h-screen w-full bg-[linear-gradient(180deg,#041119_8%,#041119_40%,#000103_100%)] pt-16">
         <SectionTitle title="DISCOVER CHARACTERS" />
-
         <div className="flex gap-5 justify-between w-full px-12 relative h-full pt-20">
           <div className="w-150 flex flex-col justify-between">
             <SectionDetail
@@ -55,6 +51,23 @@ function App() {
           src="public/images/section2bg.png"
           className="w-[70%] absolute right-0 bottom-0 opacity-50"
         />
+        <div className="absolute w-full h-20 bg-gradient-to-t from-[#000103] to-transparent  left-0 bottom-0 z-60" />
+      </section>
+
+      {/* Spells Section */}
+      <section className="h-screen bg-[#000103] pt-16">
+        <SectionTitle
+          title="LEARN WICKED SPELLS"
+          backgroundColor={"bg-hp-dark"}
+        />
+        <div className="flex gap-5 w-full px-12 relative justify-center h-full pt-8">
+          <div className="w-150 flex flex-col justify-between">
+            <SectionDetail
+              descriptionAllignment="text-center"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod."
+            />
+          </div>
+        </div>
       </section>
     </main>
   );
