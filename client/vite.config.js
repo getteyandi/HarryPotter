f58645eba8b9d741c92c7f858fd2573d5c567efd
Jhanuz <jhanuz321@gmail.com>
2025-10-29 19:10:58 +0800

    Initialized Server

---
+++ client/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import autoprefixer from "autoprefixer";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), autoprefixer()],
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
